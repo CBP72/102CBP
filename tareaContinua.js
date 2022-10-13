377807f750acf5ea1b9549c91d52ffaee51c8ab0
Carlos <Carlos.b.puch@gmail.com>
2022-10-13 09:52:26 +0200

    ejercicio3

--- tareaContinua.js
+++ tareaContinua.js
@@ -1,19 +1,35 @@
-let nombre1= "Maxwell Wrigth";
-let nombre2= "Raja Villareal";
-let nombre3= "Helen Richards";
+//creo los arrys contacto con sus propiedades
+let contacto1=["Maxwell Wrigth", "(0191)7196495","Curabitur.egestas.nunc@nonummyac.co.uk"];
+let contacto2=[ "Raja Villareal","0866 398 2895", "posure.vulputate@sed.com"]
+let contacto3=["Helen Richards","0800 1111","libero@convallis.edu"];
+//creo la lista contactos y le añado 3 contactos
+let listaContacto=[contacto1,contacto2,contacto3];
 
-let numero1= "(0191)7196495";
-let numero2= "0866 398 2895";
-let numero3= "0800 1111";
-
-let correo1= "Curabitur.egestas.nunc@nonummyac.co.uk";
-let correo2= "posure.vulputate@sed.com";
-let correo3= "libero@convallis.edu";
+//nos pide crear el 4 contacto
+let contacto4=["Maisie Haley","0913 531 3030","risus.Quisque@urna.ca"];
+//metemos el 4 contacto el la lista de contactos
+listaContacto.push(contacto4);
 
 
-console.log(nombre1,"  ",numero1,"  ",correo1);
-console.log(nombre2,"  ",numero2,"  ",correo2);
-console.log(nombre3,"  ",numero3,"  ",correo3);
+contacto5=[String(prompt("Nombre y apellido")),String(prompt("Numero telefono")),String(prompt("Correo electronico"))];
+listaContacto.push(contacto5);
+//bucle para enseñar los contactos
+for(let i=0;i<listaContacto.length;i++){
+    //if para enseñar el primero y el ultimo
+    if(i==0||i==listaContacto.length-1){
+        document.write("<p style=\"color:white\">")
+        let mostrar="";
+        for(let j=0;j<listaContacto[i].length;j++){
+            if(j<listaContacto[i].length-1)
+                mostrar=mostrar+listaContacto[i][j]+" / ";
+            else
+                mostrar=mostrar+listaContacto[i][j];
+            }//for(j)
+        document.write(mostrar);
+        console.log(mostrar);
+        alert(mostrar);
+        document.write("</p>")
+    }//if
 
-//ejercicio 1
+}//for (i)
 
